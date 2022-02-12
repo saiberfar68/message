79c18e42c155e869f5f9c5c77d8b9b04e91f0a26
Saeed Hasani Borzadaran <hassanisaeed19@yahoo.com>
2022-02-12 19:06:34 +0330

    fix: api version added

--- payam/urls.py
+++ payam/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('message.urls')),
+    path('api/v1/', include('message.urls')),
     url(r'^api-auth/', include('rest_framework.urls')),
 ]
 
